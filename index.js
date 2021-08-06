248d98d79322aa8056ae8523c94ac59fffb3c1b7
wintercricket <hodhodhod2003@gmail.com>
2021-08-06 18:28:35 -0500

    create a renderLeads function to display leads saved

--- index.js
+++ index.js
@@ -3,12 +3,33 @@
 let myLeads =[];
 let inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+let leadsEl = document.getElementById("leads-el");
+
+
+function renderLeads(){
+	leadsEl.textContent = "Leads: " ;
+	for (let i = 0; i < myLeads.length; i++){
+
+		leadsEl.textContent +=  myLeads[i] + "   ";
+	}
+}
+
 
 inputBtn.addEventListener("click", function(){
 	myLeads.push(inputEl.value);
 	console.log(myLeads);
+	renderLeads();
 });
+	
 
 
-//let result = document.querySelector("#result-el");
 
+
+
+
+// function newCard(){
+	
+// 	cards.push(card);
+// } 
+
+	
