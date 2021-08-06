4dce0cdebd0fc2950b6f7e4ecfb2a938b6c46a6d
wintercricket <hodhodhod2003@gmail.com>
2021-08-06 17:57:45 -0500

    discovered dot value to docgetelby

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 
 let myLeads =[];
-const inputEl = document.getElementById("input-el");
+let inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 
 inputBtn.addEventListener("click", function(){
-	myLeads.push("www.awesomelead.com");
+	myLeads.push(inputEl.value);
 	console.log(myLeads);
 });
 
