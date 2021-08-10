000b5291ba9aac42d7d4452e9b361616864e9503
wintercricket <hodhodhod2003@gmail.com>
2021-08-09 21:25:25 -0500

    got that delete all btn working

--- index.js
+++ index.js
@@ -15,11 +15,19 @@ const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 if(leadsFromLocalStorage){
 	myLeads = leadsFromLocalStorage
+	renderLeads()
+
 }
-renderLeads()
+
+deleteBtn.addEventListener("click", function() {
+   
+   localStorage.clear()	
+   myLeads = []
+   renderLeads()    
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
@@ -27,23 +35,13 @@ inputBtn.addEventListener("click", function() {
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
    	
 
-   	console.log(myLeads)
-    renderLeads()
-
-    
-})
-
-deleteBtn.addEventListener("click", function() {
-    myLeads.clear(inputEl.value)
-    inputEl.value = "" 
-    localStorage.clear()
    	
-
-   	console.log(myLeads)
     renderLeads()
 
     
 })
+
+
 
 function renderLeads() {
     let listItems = ""
